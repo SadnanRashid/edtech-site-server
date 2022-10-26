7fc0326338f9024d67b1c562c50daa856150d24f
SadnanRashid <sadnan.rashid.07@gmail.com>
2022-10-26 14:17:44 +0600

    adding courses

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(cors());
 //
 // load all data necessary
-const news = require("./data/news.json");
-
+const courses = require("./data/course.json");
+const cat = require("./data/course.categories.json");
 //End of data loading
 //
 
@@ -18,6 +18,10 @@ app.get("/", async (req, res) => {
   res.send("Programming-Hero Assignment10 Server Side");
 });
 
-app.get("/news", async (req, res) => {
-  res.send(news);
+app.get("/courses", async (req, res) => {
+  res.send(courses);
+});
+
+app.get("/courses/categories", async (req, res) => {
+  res.send(cat);
 });
