c156fb2ce776e696fe47af9a57f9be4d4193e01a
SadnanRashid <sadnan.rashid.07@gmail.com>
2022-10-26 13:56:42 +0600

    added categories

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const app = express();
 app.use(cors());
 //
 // load all data necessary
+const news = require("./data/news.json");
 
+//End of data loading
 //
+
 app.listen(4000, () => {
   console.log("Server Works !!! At port 4000");
 });
 
 app.get("/", async (req, res) => {
-  res.send(news_data);
+  res.send("Programming-Hero Assignment10 Server Side");
 });
 
 app.get("/news", async (req, res) => {
-  res.send("news");
+  res.send(news);
 });
