a8b913bc51968bd1afa9b536ee2da31d025c58fc
SadnanRashid <sadnan.rashid.07@gmail.com>
2022-10-27 19:05:51 +0600

    finialize

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ app.use(cors());
 // load all data necessary
 const courses = require("./data/course.json");
 const cat = require("./data/course.categories.json");
+// Below are test includes
+// const id1 = require("./data/categories/web-development.json");
+// const id2 = require("./data/categories/software-development.json");
+// const id3 = require("./data/categories/ai.json");
+// const id4 = require("./data/categories/data-science.json");
+// const id5 = require("./data/categories/embedded.json");
+// const id6 = require("./data/categories/uiux.json");
 //End of data loading
 //
 
@@ -25,3 +32,37 @@ app.get("/courses", async (req, res) => {
 app.get("/courses/categories", async (req, res) => {
   res.send(cat);
 });
+
+// Api for getting specifiv Course
+app.get("/courses/get/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = courses.find((n) => n.id == id);
+  res.send(selectedCourse);
+});
+
+// Api for getting specific Category
+app.get("/courses/category/:id", (req, res) => {
+  const id = req.params.id;
+  const categoryCourse = courses.filter((n) => n.id == id);
+  res.send(categoryCourse);
+});
+
+// //for categories (TEST!)
+// app.get("/courses/categories/1", async (req, res) => {
+//   res.send(id1);
+// });
+// app.get("/courses/categories/2", async (req, res) => {
+//   res.send(id2);
+// });
+// app.get("/courses/categories/3", async (req, res) => {
+//   res.send(id3);
+// });
+// app.get("/courses/categories/4", async (req, res) => {
+//   res.send(id4);
+// });
+// app.get("/courses/categories/5", async (req, res) => {
+//   res.send(id5);
+// });
+// app.get("/courses/categories/6", async (req, res) => {
+//   res.send(id6);
+// });
